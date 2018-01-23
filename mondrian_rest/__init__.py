7979a2c82a339f749ddf2812519f9afb3e3bd34d
Carlos Navarrete <5233593+cnavarreteliz@users.noreply.github.com>
2018-01-23 13:05:39 -0300

    Update __init__.py
    Fix import error in python3

--- mondrian_rest/__init__.py
+++ mondrian_rest/__init__.py
@@ -1,3 +1,3 @@
-from client import Cube, MondrianClient
-from aggregation import Aggregation
-from identifier import Identifier
+from mondrian_rest.client import Cube, MondrianClient
+from mondrian_rest.aggregation import Aggregation
+from mondrian_rest.identifier import Identifier
