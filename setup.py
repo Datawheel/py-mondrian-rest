2339108c735987858b4c07f072a131a00c8fadeb
Carlos Navarrete <cnavarreteliz@gmail.com>
2018-05-03 12:30:44 -0300

    masive update in mondrianr-rest

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup
 
-setup(name='mondrian_rest',
-      version='0.1',
-      description='Python client for mondrian-rest',
-      url='http://github.com/jazzido/py-mondrian-rest',
-      author='Manuel Aristaran',
-      author_email='manuel@jazzido.com',
-      license='MIT',
-      packages=['mondrian_rest'],
-      install_requires=[
-          'numpy',
-          'pandas',
-          'requests'
-      ],
-      zip_safe=False)
+setup(
+    name='mondrian-rest',
+    version='0.1',
+    description='Python client for mondrian-rest',
+    url='http://github.com/Datawheel/mondrian-rest',
+    author='Manuel Aristaran - Datawheel',
+    author_email='manuel@jazzido.com',
+    license='MIT',
+    packages=['mondrian-rest'],
+    install_requires=['numpy', 'pandas', 'requests'],
+    zip_safe=False)
