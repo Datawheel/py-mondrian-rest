7fbc4f25ae22e39c36c6f4174dc4b1612058058b
Carlos Navarrete <cnavarreteliz@gmail.com>
2018-06-19 11:12:41 -0400

    update library

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='mondrian-rest',
-    version='0.1',
+    version='0.3',
     description='Python client for mondrian-rest',
     url='http://github.com/Datawheel/mondrian-rest',
     author='Manuel Aristaran - Datawheel',
     author_email='manuel@jazzido.com',
     license='MIT',
     packages=['mondrian_rest'],
-    install_requires=['numpy', 'pandas', 'requests', 'urlparse'],
+    install_requires=['numpy', 'pandas', 'requests'],
     zip_safe=False)
